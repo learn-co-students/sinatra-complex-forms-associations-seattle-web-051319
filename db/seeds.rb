a8e25133843a2ed19e1b4ba382456c7220e1a3c3
AustinBH <harlowaustin2@gmail.com>
2019-06-05 09:34:47 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+
+austin = Owner.create(name: "Austin")
+Pet.create(name: "Lucy", owner: austin)
+Pet.create(name: "Larry", owner: austin)
+Pet.create(name: "Fiyero", owner: austin)
+Pet.create(name: "Bubba", owner:austin)
